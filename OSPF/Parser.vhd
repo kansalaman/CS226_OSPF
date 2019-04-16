c60c5a7370c0b7f69426077aa7fc4b53df878669
Saksham Goel <38839591+kazikame@users.noreply.github.com>
2019-04-16 19:27:21 +0530

    parser with no warnings

--- OSPF/Parser.vhd
+++ OSPF/Parser.vhd
@@ -45,20 +45,32 @@ signal packet_length : STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
 signal p_counter, n_counter : INTEGER := 0;
 type states is (IDLE, IP_STRIP, OSPF_STRIP, HELLO, LS);
 signal p_state, n_state : states := IDLE;
-signal n_hello_out, n_ls_out : STD_LOGIC := '0';
+--signal n_hello_out, n_ls_out : STD_LOGIC := '0';
 begin
 SEQ: process(clk)
 begin
 	if (clk'event and clk = '1') then
 		p_state <= n_state;
-		hello_out <= n_hello_out;
-		ls_out <= n_ls_out;
+--		hello_out <= n_hello_out;
+--		ls_out <= n_ls_out;
 		p_counter <= n_counter;
 		if (n_state = OSPF_STRIP and n_counter = 2) then
 			typePacket <= in1;
 		end if;
 		if (n_state = OSPF_STRIP and n_counter > 2  and n_counter + 1< 5 ) then
 				packet_length <= packet_length(7 downto 0) & in1;
+		end if;
+		
+		if (n_state = HELLO) then
+			hello_out <= '1';
+		else
+			hello_out <= '0';
+		end if;
+		
+		if (n_state = LS) then
+			ls_out <= '1';
+		else
+			ls_out <= '0';
 		end if;
 	end if;
 end process; 
@@ -68,9 +80,6 @@ COMB: process(validity, p_state, in1, p_counter, typepacket, packet_length)
 begin
 	case p_state is
 		when IDLE =>
---			n_counter <= 0;
-			--typePacket := typePacket;
-			--packet_length := packet_length;
 			if (validity = '1') then
 				n_state <= IP_STRIP;
 				n_counter <= 2;
@@ -80,8 +89,6 @@ begin
 			end if;
 
 		when IP_STRIP =>
-			--typePacket := typePacket;
-			--packet_length := packet_length;
 			if p_counter = 20 then
 				n_state <= OSPF_STRIP;
 				n_counter <= 0;
@@ -93,28 +100,24 @@ begin
 		when OSPF_STRIP =>
 			n_counter <= p_counter + 1;
 			if (p_counter + 1 < 5 ) then
-				--typePacket := typePacket;
-				--packet_length := packet_length(7 downto 0) & in1;
 				n_state <= p_state;
 			elsif (p_counter + 1 > 23) then
 				--packet_length := packet_length;
 				if (typePacket = "00000001" or typePacket = "00000010") then
 					n_state <= HELLO;
-					n_hello_out <= '1';
-					n_ls_out <= '0';
+--					n_hello_out <= '1';
+--					n_ls_out <= '0';
 					--typePacket := typePacket;
 				elsif (typePacket = "00000101" or typePacket = "00000011" or typePacket = "00000100") then
 					n_state <= LS;
-					n_ls_out <= '1';
-					n_hello_out <= '0';
+--					n_ls_out <= '1';
+--					n_hello_out <= '0';
 				else
 					n_state <= OSPF_STRIP;
-					n_ls_out <= '0';
-					n_hello_out <= '0';
+--					n_ls_out <= '0';
+--					n_hello_out <= '0';
 				end if;
 			elsif (validity = '1') then
-				--typePacket := typePacket;
-				--packet_length := packet_length;
 				n_state <= p_state;
 			else
 				--typePacket := typePacket;
@@ -125,10 +128,10 @@ begin
 		when HELLO =>
 			--typePacket := typePacket;
 			--packet_length := packet_length;
-			n_hello_out <= '1';
+--			n_hello_out <= '1';
 --			n_counter <= p_counter + 1;
 			if  p_counter + 1 > packet_length then
-				n_hello_out <= '0';
+--				n_hello_out <= '0';
 				n_counter <= 0;
 				if (validity = '1') then
 					n_state <= IP_STRIP;
@@ -143,10 +146,10 @@ begin
 		when others =>
 			--typePacket := typePacket;
 			--packet_length := packet_length;
-			n_ls_out <= '1';
+--			n_ls_out <= '1';
 --			n_counter <= p_counter + 1;
 			if  p_counter + 1 > packet_length then
-				n_ls_out <= '0';
+--				n_ls_out <= '0';
 				if (validity = '1') then
 					n_state <= IP_STRIP;
 					n_counter <= 1;
