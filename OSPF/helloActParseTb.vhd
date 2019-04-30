9d16ec5e88c52e7e0680edd49999694ff4c4134c
Yash Sharma <yash.sharma200999@gmail.com>
2019-04-30 22:34:24 +0530

    works with small testbench

--- OSPF/helloActParseTb.vhd
+++ OSPF/helloActParseTb.vhd
@@ -2,7 +2,7 @@
 -- Company: 
 -- Engineer:
 --
--- Create Date:   15:36:21 04/30/2019
+-- Create Date:   19:38:14 04/30/2019
 -- Design Name:   
 -- Module Name:   /home/yash20/isefiles/CS226_OSPF/OSPF/helloActParseTb.vhd
 -- Project Name:  OSPF
@@ -27,11 +27,12 @@
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL; 
+use IEEE.STD_LOGIC_ARITH.ALL;
+
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --USE ieee.numeric_std.ALL;
-
+ 
 ENTITY helloActParseTb IS
 END helloActParseTb;
  
@@ -43,11 +44,13 @@ ARCHITECTURE behavior OF helloActParseTb IS
     PORT(
          clk : IN  std_logic;
          self : IN  std_logic_vector(31 downto 0);
+         routerid_val : IN  std_logic;
          in1 : IN  std_logic_vector(7 downto 0);
          hellogenin : IN  std_logic;
          helloactivein : IN  std_logic;
          stateout : OUT  std_logic_vector(1 downto 0);
-         inval : IN  std_logic
+         router_id : OUT  std_logic_vector(31 downto 0);
+         in_val : IN  std_logic
         );
     END COMPONENT;
     
@@ -55,13 +58,15 @@ ARCHITECTURE behavior OF helloActParseTb IS
    --Inputs
    signal clk : std_logic := '0';
    signal self : std_logic_vector(31 downto 0) := "00000100000001010000011000000111";
+   signal routerid_val : std_logic := '0';
    signal in1 : std_logic_vector(7 downto 0) := (others => '0');
    signal hellogenin : std_logic := '0';
    signal helloactivein : std_logic := '0';
-   signal inval : std_logic := '0';
+   signal in_val : std_logic := '0';
 
     --Outputs
    signal stateout : std_logic_vector(1 downto 0);
+   signal router_id : std_logic_vector(31 downto 0);
 
    -- Clock period definitions
    constant clk_period : time := 10 ns;
@@ -72,15 +77,17 @@ BEGIN
    uut: helloActParse PORT MAP (
           clk => clk,
           self => self,
+          routerid_val => routerid_val,
           in1 => in1,
           hellogenin => hellogenin,
           helloactivein => helloactivein,
           stateout => stateout,
-          inval => inval
+          router_id => router_id,
+          in_val => in_val
         );
 
    -- Clock process definitions
-   clk_process: process
+   clk_process :process
    begin
         clk <= '0';
         wait for clk_period/2;
@@ -93,25 +100,34 @@ BEGIN
    stim_proc: process
    begin        
       -- hold reset state for 100 ns.
-      wait for 100 ns;
+      wait for 100 ns;  
 
       wait for clk_period*10;
-
-      hellogenin <= '1';
+        hellogenin <= '1';
       wait for clk_period;
       hellogenin <= '0';
-      
+      wait for clk_period;
+      routerid_val <= '1';
+      in1 <= "00001111";
+      wait for clk_period*4;
+      routerid_val <= '0';
+		in1 <= (others => '0');
       -- insert stimulus here 
       for i in 0 to 15 loop
           wait for clk_period;
-          inval <= '1';
-			 in1 <= conv_std_logic_vector(i, 8);
+          in_val <= '1';
+             in1 <= conv_std_logic_vector(i, 8);
       end loop;
-        inval <= '0';
+		wait for clk_period;
+		in_val <= '0';
+		in1 <= (others => '0');
+		wait for clk_period;
       helloactivein <= '1';
-       wait for clk_period;
-       helloactivein <= '0';
-       wait for clk_period;
+      wait for clk_period;
+      helloactivein <= '0';
+      wait for clk_period;
+      -- insert stimulus here 
+
       wait;
    end process;
 
