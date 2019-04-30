8268655dd7b1b2085222969982e376fc630141db
AnshKhurana <anshkhurana1507@gmail.com>
2019-04-30 15:50:45 +0530

    Updates hellodead

--- OSPF/hellodead.vhd
+++ OSPF/hellodead.vhd
@@ -14,16 +14,22 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity hellodead is
 port (  routerdeadinterval : in STD_LOGIC_VECTOR(31 downto 0);
-			val : in std_logic;
-			in1 : in STD_LOGIC_VECTOR(7 downto 0);
-			routerid : in STD_LOGIC_VECTOR(31 downto 0);
-			active : out std_logic);
+	val : in STD_LOGIC; -- The parser tells that it has read the new-packet info properly, so maintain a local copy of the neighbor's neigh and compare with that if the timer strikes and the packet is currently being read.
+	myrouterid : in STD_LOGIC_VECTOR(31 downto 0);
+	readneigh : in STD_LOGIC_VECTOR(31 downto 0);
+	active : out std_logic);
 end hellodead;
 
 architecture Behavioral of hellodead is
 signal isactive : std_logic := '0';
+signal p_state, n_state: states := WAITING;
+signal counter = routerdeadinterval
+
 begin
-
-
+-- Hello Parser keeps the value mapped to some variable permanently and changes the value if necessary.
+-- Actually use some field that is always renewed to ensure that the mapped values have been updated.
+-- calculate time at which the neighbour value is to be checked == routerdeadinterval
+-- change the mapped value of down signal iff neighbour's hello-packet content neighvalue does not match my routerID. 
+-- the down signal is always set to some value but will be considered only if the main FSM is in its two-way
 end Behavioral;
 
