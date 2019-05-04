2f3356fdfeb1ca65dff9f4ae75198e8cdcf08680
Yash Sharma <yash.sharma200999@gmail.com>
2019-05-04 17:42:56 +0530

    changes Tb template

--- OSPF/helloActParseTb.vhd
+++ OSPF/helloActParseTb.vhd
@@ -43,88 +43,88 @@ ARCHITECTURE behavior OF helloActParseTb IS
  
     COMPONENT helloActParse
     PORT(
-         clk =>  ,
-         neighbor_val =>  ,
-         self =>  ,
-         routerid_val =>  ,
-         dbd_val =>  ,
-         in1 =>  ,
-         hellogenin =>  ,
-         stateout =>  ,
-         router_id =>  ,
-         out1 =>  ,
-         dbd_outval =>  ,
-         lsr_outval =>  ,
-         dbd_rd_en =>  ,
-         dbd_rst =>  ,
-         numLSA =>  ,
-         dbd_out =>  ,
-         dbd_valid =>  ,
-         dbd_empty =>  ,
-         lsa_queue_wr_en =>  ,
-         lsa_queue_dout =>  ,
-         lsa_queue_rd_en =>  ,
-         lsa_queue_din =>  ,
-         lsa_queue_val =>  ,
+         clk : IN  std_logic;
+         neighbor_val : IN  std_logic;
+         self : IN  std_logic_vector(31 downto 0);
+         routerid_val : IN  std_logic;
+         dbd_val : IN  std_logic;
+         in1 : IN  std_logic_vector(7 downto 0);
+         hellogenin : IN  std_logic;
+         stateout : OUT  std_logic_vector(2 downto 0);
+         router_id : OUT  std_logic_vector(31 downto 0);
+         out1 : OUT  std_logic_vector(7 downto 0);
+         dbd_outval : OUT  std_logic;
+         lsr_outval : OUT  std_logic;
+         dbd_rd_en : OUT  std_logic;
+         dbd_rst : OUT  std_logic;
+         numLSA : IN  std_logic_vector(1 downto 0);
+         dbd_out : IN  std_logic_vector(7 downto 0);
+         dbd_valid : IN  std_logic;
+         dbd_empty : IN  std_logic;
+         lsa_queue_wr_en : OUT  std_logic;
+         lsa_queue_dout : OUT  std_logic_vector(7 downto 0);
+         lsa_queue_rd_en : OUT  std_logic;
+         lsa_queue_din : IN  std_logic_vector(7 downto 0);
+         lsa_queue_val : IN  std_logic;
          lsa_queue_empty : IN  std_logic
         );
     END COMPONENT;
     COMPONENT LSAFetcher
     PORT(
-         clk =>  ,
-         rd_en =>  ,
-         rst =>  ,
-         num_lsa =>  ,
-         db_rd_en =>  ,
-         db_addr =>  ,
-         db_din =>  ,
-         dout =>  ,
-         db_busy =>  ,
-         rd_val =>  ,
+         clk : IN  std_logic;
+         rd_en : IN  std_logic;
+         rst : IN  std_logic;
+         num_lsa : OUT  std_logic_vector(1 downto 0);
+         db_rd_en : OUT  std_logic;
+         db_addr : OUT  std_logic_vector(11 downto 0);
+         db_din : IN  std_logic_vector(7 downto 0);
+         dout : OUT  std_logic_vector(7 downto 0);
+         db_busy : IN  std_logic;
+         rd_val : OUT  std_logic;
          empty : OUT std_logic
         );
     END COMPONENT;
 
     COMPONENT RAMDB
     PORT (
-     clka =>  ,
-     ena =>  ,
-     wea =>  ,
-     addra =>  ,
-     dina =>  ,
+     clka : IN STD_LOGIC;
+     ena : IN STD_LOGIC;
+     wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+     addra : IN STD_LOGIC_VECTOR(11 DOWNTO 0);
+     dina : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
      douta : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
     );
    END component;
    COMPONENT FIFOLSU IS
     PORT (
-      clk =>  ,
-      rst =>  ,
-      din =>  ,
-      wr_en =>  ,
-      rd_en =>  ,
-      dout =>  ,
-      full =>  ,
-      empty =>  ,
+      clk : IN STD_LOGIC;
+      rst : IN STD_LOGIC;
+      din : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+      wr_en : IN STD_LOGIC;
+      rd_en : IN STD_LOGIC;
+      dout : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+      full : OUT STD_LOGIC;
+      empty : OUT STD_LOGIC;
       data_count : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)
     );
    END COMPONENT;
     
 
    --Inputs
-   signal clk =>  ,
-   signal neighbor_val =>  ,
+   signal clk : std_logic := '0';
+   signal neighbor_val : std_logic := '0';
    signal self : std_logic_vector(31 downto 0) := (others => '0');
-   signal routerid_val =>  ,
-   signal dbd_val =>  ,
+   signal routerid_val : std_logic := '0';
+   signal dbd_val : std_logic := '0';
    signal in1 : std_logic_vector(7 downto 0) := (others => '0');
-   signal hellogenin =>  ,
+   signal hellogenin : std_logic := '0';
    signal numLSA : std_logic_vector(1 downto 0) := (others => '0');
    signal dbd_out : std_logic_vector(7 downto 0) := (others => '0');
-   signal dbd_valid =>  ,
-   signal dbd_empty =>  ,
+   signal dbd_valid : std_logic := '0';
+   signal dbd_empty : std_logic := '0';
    signal lsa_queue_din : std_logic_vector(7 downto 0) := (others => '0');
-   signal lsa_queue_val =>  ,
-   signal lsa_queue_empty =>  ,
+   signal lsa_queue_val : std_logic := '0';
+   signal lsa_queue_empty : std_logic := '0';
 
  	--Outputs
    signal stateout : std_logic_vector(2 downto 0);
@@ -144,7 +144,7 @@ ARCHITECTURE behavior OF helloActParseTb IS
    signal db_din : STD_LOGIC_VECTOR(7 downto 0);
 
    -- Clock period definitions
-   constant clk_period =>  ,
+   constant clk_period : time := 10 ns;
  
 BEGIN
  
@@ -184,28 +184,28 @@ BEGIN
           douta => db_din
      );
    queue : FIFOLSU PORT MAP(
-          clk =>  ,
-          rst =>  ,
-          din =>  ,
-          wr_en =>  ,
-          rd_en =>  ,
-          dout =>  ,
-          full =>  ,
-          empty =>  ,
-          data_count => 
+          clk => clk,
+          rst => '0',
+          din =>  lsa_queue_dout,
+          wr_en => lsa_queue_wr_en,
+          rd_en => lsa_queue_rd_en,
+          dout =>  lsa_queue_din,
+          full =>  open,
+          empty =>  lsa_queue_empty,
+          data_count => open
     );
    fetcher : LSAFetcher PORT MAP(
-          clk =>  ,
-          rd_en =>  ,
-          rst =>  ,
-          num_lsa =>  ,
-          db_rd_en =>  ,
-          db_addr =>  ,
-          db_din =>  ,
-          dout =>  ,
-          db_busy =>  ,
-          rd_val =>  ,
-          empty : => 
+          clk => ,
+          rd_en => ,
+          rst => ,
+          num_lsa => ,
+          db_rd_en => ,
+          db_addr => ,
+          db_din => ,
+          dout => ,
+          db_busy => ,
+          rd_val => ,
+          empty => 
     );
    -- Clock process definitions
    clk_process :process
