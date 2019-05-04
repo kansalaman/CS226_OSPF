6eb17e39c4ce32cf13d0f0e48352883e1942bd98
Yash Sharma <yash.sharma200999@gmail.com>
2019-05-04 17:38:53 +0530

    changes Tb template

--- OSPF/helloActParseTb.vhd
+++ OSPF/helloActParseTb.vhd
@@ -43,49 +43,88 @@ ARCHITECTURE behavior OF helloActParseTb IS
  
     COMPONENT helloActParse
     PORT(
-         clk : IN  std_logic;
-         neighbor_val : IN  std_logic;
-         self : IN  std_logic_vector(31 downto 0);
-         routerid_val : IN  std_logic;
-         dbd_val : IN  std_logic;
-         in1 : IN  std_logic_vector(7 downto 0);
-         hellogenin : IN  std_logic;
-         stateout : OUT  std_logic_vector(2 downto 0);
-         router_id : OUT  std_logic_vector(31 downto 0);
-         out1 : OUT  std_logic_vector(7 downto 0);
-         dbd_outval : OUT  std_logic;
-         lsr_outval : OUT  std_logic;
-         dbd_rd_en : OUT  std_logic;
-         dbd_rst : OUT  std_logic;
-         numLSA : IN  std_logic_vector(1 downto 0);
-         dbd_out : IN  std_logic_vector(7 downto 0);
-         dbd_valid : IN  std_logic;
-         dbd_empty : IN  std_logic;
-         lsa_queue_wr_en : OUT  std_logic;
-         lsa_queue_dout : OUT  std_logic_vector(7 downto 0);
-         lsa_queue_rd_en : OUT  std_logic;
-         lsa_queue_din : IN  std_logic_vector(7 downto 0);
-         lsa_queue_val : IN  std_logic;
+         clk =>  ,
+         neighbor_val =>  ,
+         self =>  ,
+         routerid_val =>  ,
+         dbd_val =>  ,
+         in1 =>  ,
+         hellogenin =>  ,
+         stateout =>  ,
+         router_id =>  ,
+         out1 =>  ,
+         dbd_outval =>  ,
+         lsr_outval =>  ,
+         dbd_rd_en =>  ,
+         dbd_rst =>  ,
+         numLSA =>  ,
+         dbd_out =>  ,
+         dbd_valid =>  ,
+         dbd_empty =>  ,
+         lsa_queue_wr_en =>  ,
+         lsa_queue_dout =>  ,
+         lsa_queue_rd_en =>  ,
+         lsa_queue_din =>  ,
+         lsa_queue_val =>  ,
          lsa_queue_empty : IN  std_logic
         );
     END COMPONENT;
+    COMPONENT LSAFetcher
+    PORT(
+         clk =>  ,
+         rd_en =>  ,
+         rst =>  ,
+         num_lsa =>  ,
+         db_rd_en =>  ,
+         db_addr =>  ,
+         db_din =>  ,
+         dout =>  ,
+         db_busy =>  ,
+         rd_val =>  ,
+         empty : OUT std_logic
+        );
+    END COMPONENT;
+
+    COMPONENT RAMDB
+    PORT (
+     clka =>  ,
+     ena =>  ,
+     wea =>  ,
+     addra =>  ,
+     dina =>  ,
+     douta : OUT STD_LOGIC_VECTOR(7 DOWNTO 0)
+    );
+   END component;
+   COMPONENT FIFOLSU IS
+    PORT (
+      clk =>  ,
+      rst =>  ,
+      din =>  ,
+      wr_en =>  ,
+      rd_en =>  ,
+      dout =>  ,
+      full =>  ,
+      empty =>  ,
+      data_count : OUT STD_LOGIC_VECTOR(11 DOWNTO 0)
+    );
+   END COMPONENT;
     
 
    --Inputs
-   signal clk : std_logic := '0';
-   signal neighbor_val : std_logic := '0';
+   signal clk =>  ,
+   signal neighbor_val =>  ,
    signal self : std_logic_vector(31 downto 0) := (others => '0');
-   signal routerid_val : std_logic := '0';
-   signal dbd_val : std_logic := '0';
+   signal routerid_val =>  ,
+   signal dbd_val =>  ,
    signal in1 : std_logic_vector(7 downto 0) := (others => '0');
-   signal hellogenin : std_logic := '0';
+   signal hellogenin =>  ,
    signal numLSA : std_logic_vector(1 downto 0) := (others => '0');
    signal dbd_out : std_logic_vector(7 downto 0) := (others => '0');
-   signal dbd_valid : std_logic := '0';
-   signal dbd_empty : std_logic := '0';
+   signal dbd_valid =>  ,
+   signal dbd_empty =>  ,
    signal lsa_queue_din : std_logic_vector(7 downto 0) := (others => '0');
-   signal lsa_queue_val : std_logic := '0';
-   signal lsa_queue_empty : std_logic := '0';
+   signal lsa_queue_val =>  ,
+   signal lsa_queue_empty =>  ,
 
  	--Outputs
    signal stateout : std_logic_vector(2 downto 0);
@@ -99,8 +138,13 @@ ARCHITECTURE behavior OF helloActParseTb IS
    signal lsa_queue_dout : std_logic_vector(7 downto 0);
    signal lsa_queue_rd_en : std_logic;
 
+
+   --EXTRAS
+   signal db_addr : STD_LOGIC_VECTOR(11 downto 0);
+   signal db_din : STD_LOGIC_VECTOR(7 downto 0);
+
    -- Clock period definitions
-   constant clk_period : time := 10 ns;
+   constant clk_period =>  ,
  
 BEGIN
  
@@ -131,7 +175,38 @@ BEGIN
           lsa_queue_val => lsa_queue_val,
           lsa_queue_empty => lsa_queue_empty
         );
-
+   ram : RAMDB PORT MAP(
+          clka => clk,
+          ena => '1',
+          wea => "0",
+          addra => db_addr,
+          dina => (others => '0'),
+          douta => db_din
+     );
+   queue : FIFOLSU PORT MAP(
+          clk =>  ,
+          rst =>  ,
+          din =>  ,
+          wr_en =>  ,
+          rd_en =>  ,
+          dout =>  ,
+          full =>  ,
+          empty =>  ,
+          data_count => 
+    );
+   fetcher : LSAFetcher PORT MAP(
+          clk =>  ,
+          rd_en =>  ,
+          rst =>  ,
+          num_lsa =>  ,
+          db_rd_en =>  ,
+          db_addr =>  ,
+          db_din =>  ,
+          dout =>  ,
+          db_busy =>  ,
+          rd_val =>  ,
+          empty : => 
+    );
    -- Clock process definitions
    clk_process :process
    begin
